b98a5503640559ecc9af8353ffe0827959988014
Alec Forston <155654009+alecforston@users.noreply.github.com>
2025-06-03 11:42:48 -0600

    Update Masking_and_Figures.sh

--- Stratified PCA and Masked t-SNE/Pipeline_2/Masking_and_Figures.sh
+++ Stratified PCA and Masked t-SNE/Pipeline_2/Masking_and_Figures.sh
@@ -5,10 +5,6 @@
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --mem=200G   # memory per CPU core
 #SBATCH -J "Chunking And Imputing Final"   # job name
-#SBATCH --mail-user=alecsf@byu.edu   # email address
-#SBATCH --mail-type=BEGIN
-#SBATCH --mail-type=END
-#SBATCH --mail-type=FAIL
 
 # Set the max number of threads to use for programs using OpenMP. Should be <= ppn. Does nothing if the program doesn't use OpenMP.
 export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
