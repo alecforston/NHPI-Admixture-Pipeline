01bf790875af95690619fa779467953f2d426d01
Alec Forston <155654009+alecforston@users.noreply.github.com>
2025-06-05 13:21:52 -0600

    Update Run_Pipeline_2.sh

--- Stratified PCA and Masked t-SNE/Pipeline_2/Run_Pipeline_2.sh
+++ Stratified PCA and Masked t-SNE/Pipeline_2/Run_Pipeline_2.sh
@@ -2,20 +2,20 @@
 
 conda activate Capstone_Final
 
-##Converts VCF File to Plink Format 
+# Convert VCF File to Plink Format 
 plink --vcf ../african_subset_chr2.vcf.gz --make-bed --out Plink_African_Subset_Check_LD_Update
 
-#Generates a list of SNPs that should be pruned called pruned_sites_African_Subset_LD_Update.prine.in
+# Generate a list of SNPs that should be pruned called pruned_sites_African_Subset_LD_Update.prine.in
 plink --bfile Plink_African_Subset_Check_LD_Update --indep-pairwise 50 10 .2 --out pruned_sites_African_Subset_LD_Update
 
 ##This takes the Plink file Plink_African_Subset_Check_LD_Update and the SNPs to be pruned in the file pruned_sites_African_Subset_LD_Update.prune.in
 ##and produces a Plink file called African_Subset_ld_filtered_data_LD_Update
 plink --bfile Plink_African_Subset_Check_LD_Update --extract pruned_sites_African_Subset_LD_Update.prune.in --make-bed --out African_Subset_ld_filtered_data_LD_Update
 
-##This command takes the Plink file input African_Subset_ld_filtered_data_LD_Update and recodes it to a VCF called African_Subset_ld_filtered_data_LD_Update_VCF
+# Takes the Plink file input African_Subset_ld_filtered_data_LD_Update and recode it to a VCF called African_Subset_ld_filtered_data_LD_Update_VCF
 plink --bfile African_Subset_ld_filtered_data_LD_Update --recode vcf --out African_Subset_ld_filtered_data_LD_Update_VCF
 
-#These commands take the Plink file African_Subset_ld_filtered_data_LD_Update and then produces pca eiganvalues and eiganvectors called GCTA_Plink_AfricanSubset_Results_LD_Pruned_Updated.eigenval and GCTA_Plink_AfricanSubset_Results_LD_Pruned_Updated.eigenvec. This is for our data before we have performed masking. 
+# These commands take the Plink file African_Subset_ld_filtered_data_LD_Update and then produces pca eiganvalues and eiganvectors called GCTA_Plink_AfricanSubset_Results_LD_Pruned_Updated.eigenval and GCTA_Plink_AfricanSubset_Results_LD_Pruned_Updated.eigenvec. This is for our data before we have performed masking. 
 gcta --bfile African_Subset_ld_filtered_data_LD_Update --make-grm --out GCTA_African_Subset_ld_filtered_data_LD_Update
 gcta --grm GCTA_African_Subset_ld_filtered_data_LD_Update --pca 20 --out GCTA_Plink_AfricanSubset_Results_LD_Pruned_Updated
 
